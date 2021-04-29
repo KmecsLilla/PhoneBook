b2d4a8fa20833e58cd71ca6ec20b01263a7b5607
KmecsLilla <kmecs.lilla@gmail.com>
2021-04-29 12:34:12 +0200

    Implement task Phonebook

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,20 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) throws IOException {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        FileWriter fw = new FileWriter(output);
+        for ( Map.Entry<String, String> contact : contacts.entrySet()) {
+            fw.write(contact.getKey() + ": " + contact.getValue() + "\n");
+        }
+        fw.close();
+    }
+}
